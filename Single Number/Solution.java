9e1142ed6b614d2bda82306f9f0c575996fdc077
hesham elshrkawy <hesham.45645@gmail.com>
2022-01-20 19:27:26 +0200

    Solved Single Number

---
+++ Single Number/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int output = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            output = output ^ nums[i];
+        }
+        return output;
+    }
+}
